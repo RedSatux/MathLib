8ffa93cec5263b6250b02e629be58f1a0e2e516d
RedSatux <redsatux@gmail.com>
2019-12-19 11:58:28 +0330

    UPDATE - Update popup service worker was updated.

--- src/.vuepress/components/UpdatePopup.vue
+++ src/.vuepress/components/UpdatePopup.vue
@@ -2,7 +2,7 @@
     <SWUpdatePopup v-slot="{ enabled, reload, message, buttonText }">
         <div
                 v-if="enabled"
-                class="sw-update-popup">
+                class="update-popup">
             {{ message }}<br>
             <Button :text="buttonText" theme="default" @click="reload"></Button>
         </div>
@@ -19,7 +19,7 @@
 </script>
 
 <style lang="scss">
-    .sw-update-popup {
+    .update-popup {
         right: 20px;
         bottom: 20px;
         z-index: 100;
@@ -30,5 +30,19 @@
         background-color: #f5f5f5;
         border: 15px solid transparent;
         box-shadow: 0 0.5px 0 0 #f4f4f4 inset, 0 1px 2px 0 #d2d2d2;
+    }
+
+    .update-popup > button {
+        margin-top: 0.5em;
+        padding: 0.25em 2em;
+    }
+
+    .update-popup-enter-active, .update-popup-leave-active {
+        transition: opacity 0.3s, transform 0.3s;
+    }
+
+    .update-popup-enter, .update-popup-leave-to {
+        opacity: 0;
+        transform: translate(0, 50%) scale(0.5);
     }
 </style>
