2f230edef53d3695f8609dc9f13291020bf342fa
RedSatux <redsatux@gmail.com>
2019-12-19 11:41:40 +0330

    UPDATE - Update popup service worker was customized.

--- src/.vuepress/components/SWUpdatePopup.vue
+++ src/.vuepress/components/SWUpdatePopup.vue
@@ -20,15 +20,14 @@
 
 <style lang="scss">
     .sw-update-popup {
-        z-index: 100;
-        text-align: right;
-        position: fixed;
-        bottom: 20px;
-        right: 20px;
         font-size: 20px;
-        padding: 10px;
-        background-color: #f5f5f5;
-        border: 15px solid transparent;
-        box-shadow: 0 0.5px 0 0 #f4f4f4 inset, 0 1px 2px 0 #d2d2d2;
+        right: 20px !important;
+        bottom: 20px !important;
+        z-index: 100 !important;
+        padding: 10px !important;
+        text-align: right !important;
+        background-color: #f5f5f5 !important;
+        border: 15px solid transparent !important;
+        box-shadow: 0 0.5px 0 0 #f4f4f4 inset, 0 1px 2px 0 #d2d2d2 !important;
     }
 </style>
