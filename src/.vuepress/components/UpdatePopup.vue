0e444e8cf945ebc1ea9e34abbf3f0b6d95d213f9
RedSatux <redsatux@gmail.com>
2019-12-19 12:01:24 +0330

    UPDATE - Update popup service worker was updated.

--- src/.vuepress/components/UpdatePopup.vue
+++ src/.vuepress/components/UpdatePopup.vue
@@ -31,18 +31,4 @@
         border: 15px solid transparent;
         box-shadow: 0 0.5px 0 0 #f4f4f4 inset, 0 1px 2px 0 #d2d2d2;
     }
-
-    .update-popup > button {
-        margin-top: 0.5em;
-        padding: 0.25em 2em;
-    }
-
-    .update-popup-enter-active, .update-popup-leave-active {
-        transition: opacity 0.3s, transform 0.3s;
-    }
-
-    .update-popup-enter, .update-popup-leave-to {
-        opacity: 0;
-        transform: translate(0, 50%) scale(0.5);
-    }
 </style>
